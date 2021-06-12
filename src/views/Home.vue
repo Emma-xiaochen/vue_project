4d2a5a341529677489c4551157457d74bd84cc3a
Emma-xiaochen <736202625@qq.com>
2021-06-12 15:58:29 +0800

    主页修复样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,12 +21,16 @@
       <Layout>
           <Header class="layout-header-bar header">
             <img class="logo" src="../assets/logo.png" alt="">
-            <div class="info">
               <div class="face"></div>
               <a class="esc" href="#">退出</a>
-            </div>
           </Header>
           <Content :style="{margin: '20px', background: '#fff', minHeight: '220px'}">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iure sequi a necessitatibus repellat commodi labore eaque distinctio, voluptatum culpa ipsum atque. Voluptate expedita temporibus nulla nesciunt in quasi esse!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iure sequi a necessitatibus repellat commodi labore eaque distinctio, voluptatum culpa ipsum atque. Voluptate expedita temporibus nulla nesciunt in quasi esse!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iure sequi a necessitatibus repellat commodi labore eaque distinctio, voluptatum culpa ipsum atque. Voluptate expedita temporibus nulla nesciunt in quasi esse!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iure sequi a necessitatibus repellat commodi labore eaque distinctio, voluptatum culpa ipsum atque. Voluptate expedita temporibus nulla nesciunt in quasi esse!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iure sequi a necessitatibus repellat commodi labore eaque distinctio, voluptatum culpa ipsum atque. Voluptate expedita temporibus nulla nesciunt in quasi esse!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iure sequi a necessitatibus repellat commodi labore eaque distinctio, voluptatum culpa ipsum atque. Voluptate expedita temporibus nulla nesciunt in quasi esse!
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, iure sequi a necessitatibus repellat commodi labore eaque distinctio, voluptatum culpa ipsum atque. Voluptate expedita temporibus nulla nesciunt in quasi esse!
           </Content>
       </Layout>
@@ -53,51 +57,56 @@
   }
 </script>
 
-<style lang="less">
+<style lang="less"  scoped>
   .ivu-layout-header.header {
     height: 50px;
     position: relative;
     background:#9400D3;
+
+    .logo {
+      width: 120px;
+      height: 120px;
+      position: absolute;
+      top: -42px;
+      left: -20px;
+    }
+
+    .face {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      background: url(../assets/face.png);
+      position: absolute;
+      top: 5px;
+      right: 60px;
+    }
+
+    .esc {
+      color: #fff;
+      position: absolute;
+      right: 19px;
+      top: -5px;
+    }
+
+    .esc:hover {
+      color: #2d8cf0;
+    }
   }
 
-  .ivu-layout-sider-children {
-    background: #9400D3;
-  }
+  .ivu-layout-sider {
+    background:#9400D3;
 
-  .ivu-menu.ivu-menu-dark.ivu-menu-vertical.menu-item {
-    background: #9400D3;
-  }
+    .ivu-menu.ivu-menu-dark.ivu-menu-vertical.menu-item {
+      background:#9400D3;
+    }
 
-  .ivu-menu-dark.ivu-menu-vertical .ivu-menu-item-active:not(.ivu-menu-submenu), .ivu-menu-dark.ivu-menu-vertical .ivu-menu-item-active:not(.ivu-menu-submenu):hover, .ivu-menu-dark.ivu-menu-vertical .ivu-menu-submenu-title-active:not(.ivu-menu-submenu), .ivu-menu-dark.ivu-menu-vertical .ivu-menu-submenu-title-active:not(.ivu-menu-submenu):hover {
-    background: #4B0082;
-  }
+    .ivu-menu-dark.ivu-menu-vertical .ivu-menu-item:hover, .ivu-menu-dark.ivu-menu-vertical .ivu-menu-submenu-title:hover {
+      background: #8604be;
+    }
 
-  .logo {
-    width: 120px;
-    height: 120px;
-    position: absolute;
-    top: -42px;
-    left: -20px;
-  }
+    .ivu-menu-dark.ivu-menu-vertical .ivu-menu-item-active:not(.ivu-menu-submenu), .ivu-menu-dark.ivu-menu-vertical .ivu-menu-item-active:not(.ivu-menu-submenu):hover, .ivu-menu-dark.ivu-menu-vertical .ivu-menu-submenu-title-active:not(.ivu-menu-submenu), .ivu-menu-dark.ivu-menu-vertical .ivu-menu-submenu-title-active:not(.ivu-menu-submenu):hover {
+      background:#6e069a;
+    }
 
-  .info {
-    float: right;
-  }
-
-  .face {
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    background: url(../assets/face.png);
-    position: absolute;
-    top: 5px;
-    right: 60px;
-  }
-
-  .esc {
-    color: #fff;
-    position: absolute;
-    right: 19px;
-    top: -5px;
   }
 </style>
