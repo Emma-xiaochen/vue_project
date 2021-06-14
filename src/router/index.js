beeee0622cb5ac6d47238f70966eb2a4196a8d28
Emma-xiaochen <736202625@qq.com>
2021-06-14 23:46:21 +0800

    利用导航守卫修改网页标题

--- src/router/index.js
+++ src/router/index.js
@@ -8,24 +8,41 @@ const routes = [
   {
     path: '/',
     name: 'Login',
-    component: () => import('../views/Login.vue')
+    component: () => import('../views/Login.vue'),
+    meta: {
+      title: '登录'
+    }
   },
   {
     path: '/home',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: '首页'
+    }
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/Login.vue')
+    component: () => import('../views/Login.vue'),
+    meta: {
+      title: '登录'
+    }
   }
 ]
 
 const router = new VueRouter({
+  // 配置路由和最近俺之间的应用关系
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // 从from跳转到to
+  document.title = to.matched[0].meta.title
+  console.log(to);
+  next()
 })
 
 export default router
